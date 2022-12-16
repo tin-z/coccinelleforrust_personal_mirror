0087c879230485ce08111bd668210e3cfaecca96
Tathagata Roy <tathagata.roy1278@gmail.com>
2022-12-16 14:15:59 +0530

    added comments in ast0.rs

--- src/visitor_ast0/ast0.rs
+++ src/visitor_ast0/ast0.rs
@@ -30,16 +30,16 @@ impl<'a, D> worker<D>{
 
     pub fn work_on_node(&mut self, node: Box<&dyn AstNode>, df: &mut dyn FnMut(&mut worker<D>) -> Vec<D>)
     {
-        let children: Vec<D> = vec![];
-        self.children.push(children);
+        let children: Vec<D> = vec![];//this is the vecctor of children for this node
         let func = self.func_node;
         let d = func(self, self.lindex.clone(), node, df);
         let pchildren = self.children.last_mut().unwrap();//should always have one atleast
-
         match d {
-            Some(d) => { pchildren.push(d); }
+            Some(d) => { pchildren.push(d); }//pushing this node into the children of its parents
             None => {}
         }
+        self.children.push(children);//pushed away for later use
+
     }
 
     pub fn work_on_token(&mut self, token: Option<SyntaxToken>){
@@ -47,12 +47,12 @@ impl<'a, D> worker<D>{
         let func = self.func_token;
         let d = func(self.lindex.clone(), token);
         match d {
-            Some(d) => { pchildren.push(d); }
+            Some(d) => { pchildren.push(d); }//push my token
             None => {}
         }
     }
 
     pub fn pop_children(&mut self) -> Vec<D>{
-        self.children.pop().unwrap()
+        self.children.pop().unwrap()//pop the children of the current node and return
     }
 }
