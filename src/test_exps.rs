1045bb4433dae924d22581830318c3ac491a1301
Tathagata Roy <tathagata.roy1278@gmail.com>
2022-12-19 14:59:21 +0530

    fixed formatting

--- src/test_exps.rs
+++ src/test_exps.rs
@@ -1,22 +1,22 @@
-use std::{process::Child};
+use std::process::Child;
 
-use ide_db::line_index::{LineIndex};
-use parser::SyntaxKind;
-use syntax::{AstNode, ast::AstChildren};
-use crate::wrap::{Rnode, Syntax, fill_wrap, wrap};
-pub use crate::wrap::visit_keyword;
 use crate::util::{tuple_of_2, tuple_of_3};
+pub use crate::wrap::visit_keyword;
+use crate::wrap::{fill_wrap, wrap, Rnode, Syntax};
+use ide_db::line_index::LineIndex;
+use parser::SyntaxKind;
+use syntax::{ast::AstChildren, AstNode};
 
-impl wrap{
-    pub fn set_test_exps(&mut self){
+impl wrap {
+    pub fn set_test_exps(&mut self) {
         self.true_if_test = true;
         self.true_if_test_exp = true;
     }
 }
 
-pub fn process_exp(exp: &mut Rnode){
+pub fn process_exp(exp: &mut Rnode) {
     exp.wrapper.set_test_exps();
-    match exp.astnode.kind(){
+    match exp.astnode.kind() {
         SyntaxKind::PAREN_EXPR => {
             let [_lp, exp, _rp] = tuple_of_3(&mut exp.children);
             process_exp(exp);
@@ -25,8 +25,8 @@ pub fn process_exp(exp: &mut Rnode){
     }
 }
 
-pub fn set_test_exps(node: &mut Rnode){
-    match node.astnode.kind(){
+pub fn set_test_exps(node: &mut Rnode) {
+    match node.astnode.kind() {
         SyntaxKind::IF_EXPR => {
             let [_if, cond] = tuple_of_2(&mut node.children);
             process_exp(cond);
@@ -37,16 +37,19 @@ pub fn set_test_exps(node: &mut Rnode){
         }
         SyntaxKind::BIN_EXPR => {
             let [lhs, op, rhs] = tuple_of_3(&mut node.children);
-            if op.astnode.is_relational() { process_exp(lhs); process_exp(rhs); }
-        }
-        SyntaxKind::PREFIX_EXPR//Have to be sure of this identity TODO
-            => {
-                let [op, exp] = tuple_of_2(&mut node.children);
-                if op.astnode.is_relational() { process_exp(exp); };
+            if op.astnode.is_relational() { 
+                process_exp(lhs); process_exp(rhs); 
             }
+        }
+        SyntaxKind::PREFIX_EXPR => {//Have to be sure of this identity TODO
+            let [op, exp] = tuple_of_2(&mut node.children);
+            if op.astnode.is_relational() {
+                process_exp(exp); 
+            };
+        }
         _ => { }
     }
-    for node in &mut node.children{
+    for node in &mut node.children {
         set_test_exps(node);
     }
 }
