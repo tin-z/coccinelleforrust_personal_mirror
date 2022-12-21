c7e451cd011765f729f6364565437e0c85e29381
Tathagata Roy <tathagata.roy1278@gmail.com>
2022-12-21 19:18:43 +0530

    Removed parsestmts struct and incorporated the vars inside the function

--- src/parse_cocci.rs
+++ src/parse_cocci.rs
@@ -9,62 +9,39 @@ fn get_blxpr(contents: &str) -> BlockExpr {
     Fn::cast(fnode).unwrap().body().unwrap()
 }
 
-struct ParseStmts {
-    plusstmts: String,
-    minusstmts: String,
-}
-
-impl ParseStmts {
-    pub fn new() -> ParseStmts {
-        ParseStmts {
-            plusstmts: String::from(""),
-            minusstmts: String::from(""),
-        }
-    }
-
-    fn pushplus(&mut self, s: &str) {
-        self.plusstmts.push_str(s);
-        self.plusstmts.push('\n');
-    }
-
-    fn pushminus(&mut self, s: &str) {
-        self.minusstmts.push_str(s);
-        self.minusstmts.push('\n');
-    }
-
-    fn getfuncs(&self) -> (BlockExpr, BlockExpr) {
-        let plusfn = format!("fn {}_plus {{ {} }}", "coccifn", self.plusstmts);
-        let minusfn = format!("fn {}_plus {{ {} }}", "coccifn", self.minusstmts);
-
-        (get_blxpr(&plusfn[..]), get_blxpr(&minusfn[..]))
-    }
-}
-
 pub fn parse_cocci(contents: &str) {
     let lines: Vec<String> = contents.lines().map(String::from).collect();
     let mut inmetadec = false; //checks if in metavar declaration
     let mut lino = 0; //stored line numbers
                       //mutable because I supply it with modifier statements
-    let mut parser: ParseStmts = ParseStmts::new();
+    
+    let mut plusstmts =  String::from("");
+    let mut minusstmts = String::from("");
     for line in lines {
         let mut chars = line.chars();
         match (chars.next(), chars.next(), inmetadec) {
             (Some('@'), Some('@'), false) => {
                 //starting of @@ block
-                let (pfunc, mfunc) = parser.getfuncs();
+                let plusfn = format!("fn {}_plus {{ {} }}", "coccifn", plusstmts);
+                let minusfn = format!("fn {}_plus {{ {} }}", "coccifn", minusstmts);
+                (get_blxpr(plusfn.as_str()), get_blxpr(minusfn.as_str()));//will work on these nodes
                 inmetadec = true;
             }
             (Some('@'), Some('@'), true) => {
                 //end of @@ block
                 //TODO: Handle meta variables
-                parser = ParseStmts::new();
+                plusstmts =  String::from("");
+                minusstmts = String::from("");
                 inmetadec = false;
             }
             (Some('+'), _, false) => {
-                parser.pushplus(line.as_str());
+
+                plusstmts.push_str(line.as_str());
+                plusstmts.push('\n');
             }
             (Some('-'), _, false) => {
-                parser.pushminus(line.as_str());
+                minusstmts.push_str(line.as_str());
+                minusstmts.push('\n');
             }
             (Some('+'), _, true) => {
                 panic!(
@@ -81,11 +58,18 @@ pub fn parse_cocci(contents: &str) {
                 );
             }
             _ => {
-                parser.pushplus(line.as_str());
-                parser.pushminus(line.as_str());
+                plusstmts.push_str(line.as_str());
+                plusstmts.push('\n');
+                minusstmts.push_str(line.as_str());
+                minusstmts.push('\n');
             }
         }
         lino += 1;
     }
-    let (pfunc, mfunc) = parser.getfuncs(); //getting the last rule
+
+    //takes care of the last block
+    let plusfn = format!("fn {}_plus {{ {} }}", "coccifn", plusstmts);
+    let minusfn = format!("fn {}_plus {{ {} }}", "coccifn", minusstmts);
+    (get_blxpr(plusfn.as_str()), get_blxpr(minusfn.as_str()));//will work on these functions
 }
+
