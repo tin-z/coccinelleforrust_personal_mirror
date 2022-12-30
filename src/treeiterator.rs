14a26e3713d5c28bc30708121a45704be223ea48
Tathagata Roy <tathagata.roy1278@gmail.com>
2022-12-30 16:23:00 +0100

    Added the tree iterator

--- src/treeiterator.rs
+++ src/treeiterator.rs
@@ -1,29 +1,26 @@
+use syntax::SyntaxTreeBuilder;
+
 use crate::wrap::Rnode;
 
-pub struct traveller<'a>{
-    stack: Vec<&'a Rnode>,
-    curr: &'a Rnode
+pub struct traveller<'a> {
+    vec: Vec<&'a Rnode>
 }
 
-impl<'a> traveller<'a>{
-    pub fn new(node: &'a Rnode) -> traveller{
-        traveller { stack: vec![node], curr: node }
+impl<'a> traveller<'a> {
+
+    fn buildtree(mut self, node: &'a Rnode) -> Vec<&'a Rnode>{
+        self.vec.push(node);
+        for child in &node.children_with_tokens{
+            let mut tmp = traveller::new(child);
+            self.vec.append(&mut tmp);
+        }
+        self.vec
     }
-}
 
-impl<'a> Iterator for traveller<'a>{
-    type Item = &'a Rnode;
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.stack.len() == 0{
-            return None;
-        }
-        let tmp = self.stack.pop().unwrap();
-        self.curr = tmp;
-        for child in (0..self.curr.children_with_tokens.len()).rev(){
-            self.stack.push(
-                &self.curr.children_with_tokens[child]
-            );
-        }
-        Some(tmp)
+    pub fn new(node: &'a Rnode) -> Vec<&'a Rnode> {
+        let mut t = traveller {
+            vec: vec![node]
+        };
+        t.buildtree(node)
     }
 }
