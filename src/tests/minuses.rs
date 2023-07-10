d7de5164c82ab02bbbe03d9c231e4e84e54ab954
Tathagata Roy <tathagata.roy1278@gmail.com>
2023-07-10 02:15:27 +0200

    added some more tests

--- src/tests/minuses.rs
+++ src/tests/minuses.rs
@@ -1,43 +1,44 @@
-use std::{fs, process::Command};
+#![allow(dead_code)]
+use std::{fs};
 
-use crate::engine::transformation;
+use crate::{engine::transformation, parsing_rs::{parse_rs::processrs, ast_rs::Rnode}};
 
 static PREFIX: &str = "./src/tests/";
-fn transformfile(coccifile: &str, rsfile: &str) -> String{
-    let patchstring = fs::read_to_string(format!("{}{}", PREFIX, coccifile)).expect("This shouldnt be empty");
-    let rustcode = fs::read_to_string(format!("{}{}", PREFIX, rsfile)).expect("This shouldnt be empty");
-    
-    let randfilename = String::from("/tmp/target149.rs");
-    let transformedcode = transformation::transformfile(patchstring, rustcode);
-    transformedcode.ok().unwrap().writetreetofile(&randfilename);
-    let a = Command::new("rustfmt")
-        .arg("--config-path")
-        .arg("rustfmt.toml")
-        .arg(&randfilename)
-        .output()
-        .expect("rustfmt failed");
-    
-    let data = fs::read_to_string(&randfilename).expect("Unable to read file");
-    return data;
+fn transformfile(coccifile: &str, rsfile: &str) -> Rnode {
+    let patchstring =
+        fs::read_to_string(format!("{}{}", PREFIX, coccifile)).expect("This shouldnt be empty.");
+    let rustcode =
+        fs::read_to_string(format!("{}{}", PREFIX, rsfile)).expect("This shouldnt be empty.");
+
+    let transformedcode = transformation::transformfile(patchstring, rustcode).ok().unwrap();
+    let rnode = processrs(&transformedcode.gettokenstream()).unwrap();
+    return rnode;
+}
+
+fn testtransformation(coccifile: &str, rsfile: &str, expectedfile: &str) -> bool {
+    let out = transformfile(coccifile, rsfile);
+    let expected = fs::read_to_string(format!("{}{}", PREFIX, expectedfile)).expect("This should not be empty.");
+    let rnode = processrs(&expected).unwrap();
+    return rnode.equals(&out);
 }
 
 #[test]
 pub fn test1() {
-    let out = transformfile("minuses/test1.cocci", "minuses/test1.rs");
-    println!("{}", out);
     assert!(
-        out.trim() == "fn main() {}"
-    );
+        testtransformation("minuses/test1.cocci", "minuses/test1.rs", "minuses/expected1.rs")
+    )
 }
 
 #[test]
 pub fn test2() {
-    let out = transformfile("minuses/test2.cocci", "minuses/test2.rs");
-    println!("{}", out);
     assert!(
-        out.trim() == "fn main() {
-            if e > 12 {}
-        }"
-    );
+        testtransformation("minuses/test2.cocci", "minuses/test2.rs", "minuses/expected2.rs")
+    )
 }
 
+#[test]
+pub fn test3() {
+    assert!(
+        testtransformation("minuses/test3.cocci", "minuses/test3.rs", "minuses/expected3.rs")
+    )
+}
