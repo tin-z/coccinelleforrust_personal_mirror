f614d0ecd27e7380ca73674aaef5ed2c1bc6788b
Tathagata Roy <tathagata.roy1278@gmail.com>
2023-08-03 22:46:58 +0530

    removed the expr statement

--- src/parsing_rs/parse_rs.rs
+++ src/parsing_rs/parse_rs.rs
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 
 use ra_ide_db::line_index::{LineCol, LineIndex};
-use ra_parser::SyntaxKind;
 use ra_syntax::{SourceFile, SyntaxElement, SyntaxNode};
 
 use crate::{commons::info::ParseInfo, parsing_rs::visitor_ast::work_node};
@@ -40,14 +39,6 @@ pub fn processrswithsemantics(contents: &str, rnode: SyntaxNode) -> Result<Rnode
         let children = df(&node);
         let node = if children.len() == 0 { Some(node.clone()) } else { None };
         let rnode = Rnode { wrapper: wrapped, asttoken: node, kind: kind, children: children };
-        if rnode.kind() == SyntaxKind::EXPR_STMT && rnode.children.len() == 1 {
-            // this means there is an expression statement without a ; at the end
-            //the reason these are removed because rust-analyzer seems to alter between
-            //assigning ExprStmt and IfExprs(maybe others too)
-            let mut expr = rnode.children.into_iter().next().unwrap();
-            expr.wrapper.wspaces = rnode.wrapper.wspaces;
-            return expr;
-        }
         rnode
     };
     Ok(work_node(wrap_node, String::new(), SyntaxElement::Node(rnode)))
@@ -89,14 +80,6 @@ pub fn processrs(contents: &str) -> Result<Rnode, String> {
             kind: kind,
             children: children,
         };
-        if rnode.kind() == SyntaxKind::EXPR_STMT && rnode.children.len() == 1 {
-            // this means there is an expression statement without a ; at the end
-            //the reason these are removed because rust-analyzer seems to alter between
-            //assigning ExprStmt and IfExprs(maybe others too)
-            let mut expr = rnode.children.into_iter().next().unwrap();
-            expr.wrapper.wspaces = rnode.wrapper.wspaces;
-            return expr;
-        }
         rnode
     };
     Ok(work_node(wrap_node, String::new(), SyntaxElement::Node(root)))
